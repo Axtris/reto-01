22d9a7d8d0a0908dc1053226bf0d0d9a6ab1e1ae
Juancito <juanemastrandrea@gmail.com>
2022-02-20 15:25:05 -0300

    delete unnecesary variables

--- project.py
+++ project.py
@@ -1,15 +1,24 @@
 import os
 
+# Especifico la ruta para el usuario que este usando
+
 ruta = R"C:\Users\$USERNAME\Downloads"
+
+# Tendria que hacer un control para que determine el sistema operativo
+# y que de ahi elija la variable a usar.
+
+# Con os.path.expandvars reemplazo $USERNAME por el username correspondiente
 
 Fldr = os.path.expandvars(ruta)
 
 print(Fldr)
 
-listar = Fldr
+# Usando la variable anterior, inserto una variable que me permita listar lo que haya 
+# en el directorio anterior. 
 
-dir_list = os.listdir(listar)
+dir_list = os.listdir(Fldr)
 
 print(dir_list)
 
+print(Fldr)
 
